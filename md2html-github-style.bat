8b517fe93df7cf64abcf2d12a8a2e979defc43eb
Hidetoshi <mayoiga777@gmail.com>
2022-11-12 22:14:39 +0900

    Modify batch files.
    - Add pandoc options.
    - Add echo and timeout.

--- md2html-github-style.bat
+++ md2html-github-style.bat
@@ -1,5 +1,14 @@
+@echo off
+
+echo "Converting..."
 pandoc ^
+    --quiet ^
+    -f markdown ^
+    -t html ^
     -s ^
     -c %~dp0\github-style.css ^
     "%~1" ^
     -o "%~n1.html"
+
+echo "Done."
+timeout /t 3
--- md2html-with-sidebar.bat
+++ md2html-with-sidebar.bat
@@ -1,6 +1,15 @@
+@echo off
+
+echo Converting...
 pandoc ^
+    --quiet ^
+    -f markdown ^
+    -t html ^
     -s ^
     --toc ^
     --template=%~dp0\easy-pandoc-templates\html\elegant_bootstrap_menu.html ^
     "%~1" ^
     -o "%~n1.html"
+
+echo Done.
+timeout /t 3
